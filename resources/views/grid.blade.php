aa7211abc3f7380e4cb9c83b24655908964dd324
yakabay <yakabay86@gmail.com>
2018-05-28 12:29:26 +0300

    refactor

--- resources/views/grid.blade.php
+++ resources/views/grid.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" id="app">
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-lg-10">
             
